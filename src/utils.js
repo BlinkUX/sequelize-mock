fdc884bb6ade51fbb91e6e537341c8c7ebc91e3f
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2016-10-19 12:03:01 -0700

    Fix `lowercaseFirst` undefined bug

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ var inflection = require('inflection');
 exports.uppercaseFirst = function (str) {
 	return str[0].toUpperCase() + str.slice(1);
 };
-exports.lowercaseFirst = function () {
+exports.lowercaseFirst = function (str) {
 	return str[0].toLowerCase() + str.slice(1);
 };
 exports.singularize = function(str) {
