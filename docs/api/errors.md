681f613ad932041af30783c95fa7b2a13df8b54b
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2017-02-02 17:00:29 -0800

    Error File API Documentation

---
+++ docs/api/errors.md
@@ -0,0 +1,286 @@
+# Errors
+
+Error objects are exposed via the base SequelizeMock object and can be used in much the same
+way that native Sequelize errors can be used. Because no queries are run, any errors that
+would normally contain a `sql` property include it with a value indicating this is test code
+
+*NOTE:* Error names are copied exactly from Sequelize in the off-chance code is relying on
+error name instead of equality or instanceof
+
+
+
+<a name="BaseError"></a>
+## new BaseError()
+
+BaseError is the base class most other Sequelize Mock Error classes inherit from
+Mostly based off of [the Sequelize.js equivalent code](https://github.com/sequelize/sequelize/blob/3e5b8772ef75169685fc96024366bca9958fee63/lib/errors.js#L20) <br>**Extends** Error <br>**Alias** Error
+
+
+
+<a name="ValidationError"></a>
+## new ValidationError(msg, [errors])
+
+ValidationError is the class used for validation errors returned by Sequelize Mock.
+The most common way you will see this error is using the `$addValidationError` test
+helper method on instances.
+
+ValidationErrors contain an `errors` property which contains a list of the specific
+validation errors that were encountered. For example, if two validation errors were
+encountered, it will be an array of length two. Objects in this array are of type
+`ValidationErrorItem`. <br>**Extends** [BaseError](#BaseError)
+
+**See**
+
+ - [ValidationErrorItem](#ValidationErrorItem)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+msg | String | Error Message
+[errors] | Array.&#60;ValidationErrorItem&#62; | Error Message
+
+
+
+
+<a name="errors"></a>
+### .errors
+
+Array of all validation errors that were encountered with
+this validation
+
+
+
+<a name="get"></a>
+### #get(path) -> Array.&#60;ValidationErrorItem&#62;
+
+Get the validation error for a particular field
+
+####  Parameters
+
+Name | Type | Description
+--- | --- | ---
+path | String | Field you would like the list of validation errors for
+
+
+####  Return
+Returns <Array.&#60;ValidationErrorItem&#62;> Array of validation errors for the given field
+
+
+
+<a name="ValidationErrorItem"></a>
+## new ValidationErrorItem(msg, type, path, value)
+
+A specific validation failure result
+
+**See**
+
+ - ValidationError
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+msg | String | Error Message
+type | String | Type of the error
+path | String | Field with the error
+value | String | Value with the error
+
+
+
+
+<a name="DatabaseError"></a>
+## new DatabaseError(parentError)
+
+The generic base Database Error class <br>**Extends** [BaseError](#BaseError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="sql"></a>
+### .sql
+
+Ordinarily contains the SQL that triggered the error.
+However, because test code does not go to the DB and
+therefore does not have SQL, this will contain an SQL
+comment stating it is from Sequelize Mock.
+
+
+
+<a name="TimeoutError"></a>
+## new TimeoutError()
+
+Database timeout error <br>**Extends** [DatabaseError](#DatabaseError)
+
+
+
+<a name="UniqueConstraintError"></a>
+## new UniqueConstraintError(opts)
+
+Unique constraint violation error <br>**Extends** [ValidationError](#ValidationError), [DatabaseError](#DatabaseError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+opts | Object | Unique constraint error information
+opts.message | String | Error message
+[opts.errors] | Array.&#60;ValidationErrorItem&#62; | Errors that were encountered
+[opts.fields] | Array.&#60;String&#62; | Fields that violated the unique constraint
+
+
+
+
+<a name="ForeignKeyConstraintError"></a>
+## new ForeignKeyConstraintError(opts)
+
+Foreign key constraint violation error <br>**Extends** [BaseError](#BaseError), [DatabaseError](#DatabaseError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+opts | Object | Unique constraint error information
+opts.message | String | Error message
+[opts.fields] | Array.&#60;String&#62; | Fields that violated the unique constraint
+opts.table | String | Name of the table the failure is for
+opts.value | String | Invalid value
+opts.index | String | Name of the foreign key index that was violated
+
+
+
+
+<a name="ExclusionConstraintError"></a>
+## new ExclusionConstraintError(opts)
+
+Exclusion constraint violation error <br>**Extends** [BaseError](#BaseError), [DatabaseError](#DatabaseError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+opts | Object | Unique constraint error information
+opts.message | String | Error message
+[opts.fields] | Array.&#60;String&#62; | Fields that violated the unique constraint
+opts.table | String | Name of the table the failure is for
+opts.constraint | String | Name of the constraint that was violated
+
+
+
+
+<a name="ConnectionError"></a>
+## new ConnectionError(parentError)
+
+Generic database connection error <br>**Extends** [BaseError](#BaseError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="ConnectionRefusedError"></a>
+## new ConnectionRefusedError(parentError)
+
+Database connection refused error <br>**Extends** [ConnectionError](#ConnectionError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="AccessDeniedError"></a>
+## new AccessDeniedError(parentError)
+
+Database access denied connection error <br>**Extends** [ConnectionError](#ConnectionError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="HostNotFoundError"></a>
+## new HostNotFoundError(parentError)
+
+Database host not found connection error <br>**Extends** [ConnectionError](#ConnectionError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="HostNotReachableError"></a>
+## new HostNotReachableError(parentError)
+
+Database host not reachable connection error <br>**Extends** [ConnectionError](#ConnectionError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="InvalidConnectionError"></a>
+## new InvalidConnectionError(parentError)
+
+Database invalid connection error <br>**Extends** [ConnectionError](#ConnectionError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="ConnectionTimedOutError"></a>
+## new ConnectionTimedOutError(parentError)
+
+Database connection timed out error <br>**Extends** [ConnectionError](#ConnectionError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+parentError | Error | Original error that triggered this error
+
+
+
+
+<a name="InstanceError"></a>
+## InstanceError(message)
+
+Error from a Sequelize (Mock) Instance <br>**Extends** [BaseError](#BaseError)
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+message | Error | Error message
+
+
--- src/errors.js
+++ src/errors.js
@@ -1,18 +1,28 @@
-/**
- * Errors
- *
- * NOTE: Error names are copied exactly from Sequelize in the off-chance
- *       code is relying on error name instead of equality or instanceof
- */
 'use strict';
+
+/**
+ * Error objects are exposed via the base SequelizeMock object and can be used in much the same
+ * way that native Sequelize errors can be used. Because no queries are run, any errors that
+ * would normally contain a `sql` property include it with a value indicating this is test code
+ * 
+ * *NOTE:* Error names are copied exactly from Sequelize in the off-chance code is relying on
+ * error name instead of equality or instanceof
+ * 
+ * @name Errors
+ * @fileOverview Error objects used by Sequelize and available via Sequelize Mock
+ */
 
 var util = require('util');
 var _ = require('lodash');
 
 /**
- * BaseError
- *
- * Mostly based off of https://github.com/sequelize/sequelize/blob/3e5b8772ef75169685fc96024366bca9958fee63/lib/errors.js#L20
+ * BaseError is the base class most other Sequelize Mock Error classes inherit from
+ * Mostly based off of [the Sequelize.js equivalent code](https://github.com/sequelize/sequelize/blob/3e5b8772ef75169685fc96024366bca9958fee63/lib/errors.js#L20)
+ * 
+ * @class BaseError
+ * @constructor
+ * @alias Error
+ * @extends Error
  */
 exports.BaseError = function() {
 	// Set basic error info
@@ -30,27 +40,60 @@ util.inherits(exports.BaseError, Error);
 
 
 /**
- * ValidationError
+ * ValidationError is the class used for validation errors returned by Sequelize Mock.
+ * The most common way you will see this error is using the `$addValidationError` test
+ * helper method on instances.
+ * 
+ * ValidationErrors contain an `errors` property which contains a list of the specific
+ * validation errors that were encountered. For example, if two validation errors were
+ * encountered, it will be an array of length two. Objects in this array are of type
+ * `ValidationErrorItem`.
+ * 
+ * @class ValidationError
+ * @constructor
+ * @see {@link ValidationErrorItem}
+ * @extends {@link BaseError}
+ * @param {String} msg Error Message
+ * @param {Array<ValidationErrorItem>} [errors] Error Message
  */
 exports.ValidationError = function (msg, errors) {
 	exports.BaseError.apply(this, arguments);
 	
 	this.name = 'SequelizeValidationError';
 	this.message = msg || 'Validation Error';
+	/**
+	 * Array of all validation errors that were encountered with
+	 * this validation
+	 * 
+	 * @member {Array<ValidationErrorItem>}
+	 **/
 	this.errors = errors || [];
 };
 // Extend BaseError
 util.inherits(exports.ValidationError, exports.BaseError);
 
 /**
- * ValidationError#get
+ * Get the validation error for a particular field
+ * 
+ * @memberof ValidationError
+ * @instance
+ * @param {String} path Field you would like the list of validation errors for
+ * @return {Array<ValidationErrorItem>} Array of validation errors for the given field
  */
 exports.ValidationError.prototype.get = function (path) {
 	return _.filter(this.errors, { path: path, });
 };
 
 /**
- * ValidationErrorItem
+ * A specific validation failure result
+ * 
+ * @class ValidationErrorItem
+ * @constructor
+ * @see ValidationError
+ * @param {String} msg Error Message
+ * @param {String} type Type of the error
+ * @param {String} path Field with the error
+ * @param {String} value Value with the error
  */
 exports.ValidationErrorItem = function (msg, type, path, value) {
 	this.message = msg;
@@ -61,10 +104,24 @@ exports.ValidationErrorItem = function (msg, type, path, value) {
 
 
 /**
- * DatabaseError
+ * The generic base Database Error class
+ * 
+ * @class DatabaseError
+ * @constructor
+ * @extends {@link BaseError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.DatabaseError = function (parentError) {
 	this.parent = this.original = parentError;
+	/**
+	 * Ordinarily contains the SQL that triggered the error.
+	 * However, because test code does not go to the DB and
+	 * therefore does not have SQL, this will contain an SQL
+	 * comment stating it is from Sequelize Mock.
+	 * 
+	 * @name sql
+	 * @member {string}
+	 **/
 	this.sql = '/* Sequelize Mock; No SQL generated */';
 };
 // Extend BaseError
@@ -72,7 +129,11 @@ util.inherits(exports.DatabaseError, exports.BaseError);
 
 
 /**
- * TimeoutError
+ * Database timeout error
+ * 
+ * @class TimeoutError
+ * @constructor
+ * @extends {@link DatabaseError}
  */
 exports.TimeoutError = function () {
 	exports.BaseError.call(this, 'Query Timed Out');
@@ -85,7 +146,16 @@ util.inherits(exports.TimeoutError, exports.DatabaseError);
 
 
 /**
- * UniqueConstraintError
+ * Unique constraint violation error
+ * 
+ * @class UniqueConstraintError
+ * @constructor
+ * @extends {@link ValidationError}
+ * @extends {@link DatabaseError}
+ * @param {Object} opts Unique constraint error information
+ * @param {String} opts.message Error message
+ * @param {Array<ValidationErrorItem>} [opts.errors] Errors that were encountered
+ * @param {Array<String>} [opts.fields] Fields that violated the unique constraint
  */
 exports.UniqueConstraintError = function (opts) {
 	opts = opts || {};
@@ -101,7 +171,18 @@ util.inherits(exports.UniqueConstraintError, exports.ValidationError);
 
 
 /**
- * ForeignKeyConstraintError
+ * Foreign key constraint violation error
+ * 
+ * @class ForeignKeyConstraintError
+ * @constructor
+ * @extends {@link BaseError}
+ * @extends {@link DatabaseError}
+ * @param {Object} opts Unique constraint error information
+ * @param {String} opts.message Error message
+ * @param {Array<String>} [opts.fields] Fields that violated the unique constraint
+ * @param {String} opts.table Name of the table the failure is for
+ * @param {String} opts.value Invalid value
+ * @param {String} opts.index Name of the foreign key index that was violated
  */
 exports.ForeignKeyConstraintError = function (opts) {
 	opts = opts || {};
@@ -120,7 +201,17 @@ util.inherits(exports.ForeignKeyConstraintError, exports.DatabaseError);
 
 
 /**
- * ExclusionConstraintError
+ * Exclusion constraint violation error
+ * 
+ * @class ExclusionConstraintError
+ * @constructor
+ * @extends {@link BaseError}
+ * @extends {@link DatabaseError}
+ * @param {Object} opts Unique constraint error information
+ * @param {String} opts.message Error message
+ * @param {Array<String>} [opts.fields] Fields that violated the unique constraint
+ * @param {String} opts.table Name of the table the failure is for
+ * @param {String} opts.constraint Name of the constraint that was violated
  */
 exports.ExclusionConstraintError = function (opts) {
 	opts = opts || {};
@@ -138,7 +229,12 @@ util.inherits(exports.ExclusionConstraintError, exports.DatabaseError);
 
 
 /**
- * ConnectionError
+ * Generic database connection error
+ * 
+ * @class ConnectionError
+ * @constructor
+ * @extends {@link BaseError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.ConnectionError = function (parentError) {
 	exports.BaseError.call(this, parentError ? parentError.message : 'Connection Error');
@@ -150,7 +246,12 @@ util.inherits(exports.ConnectionError, exports.BaseError);
 
 
 /**
- * ConnectionRefusedError
+ * Database connection refused error
+ * 
+ * @class ConnectionRefusedError
+ * @constructor
+ * @extends {@link ConnectionError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.ConnectionRefusedError = function (parentError) {
 	exports.ConnectionError.call(this, parentError);
@@ -161,7 +262,12 @@ util.inherits(exports.ConnectionRefusedError, exports.ConnectionError);
 
 
 /**
- * AccessDeniedError
+ * Database access denied connection error
+ * 
+ * @class AccessDeniedError
+ * @constructor
+ * @extends {@link ConnectionError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.AccessDeniedError = function (parentError) {
 	exports.ConnectionError.call(this, parentError);
@@ -172,7 +278,12 @@ util.inherits(exports.AccessDeniedError , exports.ConnectionError);
 
 
 /**
- * HostNotFoundError
+ * Database host not found connection error
+ * 
+ * @class HostNotFoundError
+ * @constructor
+ * @extends {@link ConnectionError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.HostNotFoundError = function (parentError) {
 	exports.ConnectionError.call(this, parentError);
@@ -183,7 +294,12 @@ util.inherits(exports.HostNotFoundError , exports.ConnectionError);
 
 
 /**
- * HostNotReachableError
+ * Database host not reachable connection error
+ * 
+ * @class HostNotReachableError
+ * @constructor
+ * @extends {@link ConnectionError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.HostNotReachableError = function (parentError) {
 	exports.ConnectionError.call(this, parentError);
@@ -194,7 +310,12 @@ util.inherits(exports.HostNotReachableError , exports.ConnectionError);
 
 
 /**
- * InvalidConnectionError
+ * Database invalid connection error
+ * 
+ * @class InvalidConnectionError
+ * @constructor
+ * @extends {@link ConnectionError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.InvalidConnectionError = function (parentError) {
 	exports.ConnectionError.call(this, parentError);
@@ -205,7 +326,12 @@ util.inherits(exports.InvalidConnectionError , exports.ConnectionError);
 
 
 /**
- * ConnectionTimedOutError
+ * Database connection timed out error
+ * 
+ * @class ConnectionTimedOutError
+ * @constructor
+ * @extends {@link ConnectionError}
+ * @param {Error} parentError Original error that triggered this error
  */
 exports.ConnectionTimedOutError = function (parentError) {
 	exports.ConnectionError.call(this, parentError);
@@ -216,7 +342,12 @@ util.inherits(exports.ConnectionTimedOutError , exports.ConnectionError);
 
 
 /**
- * InstanceError
+ * Error from a Sequelize (Mock) Instance
+ * 
+ * @class InstanceError
+ * @constructor
+ * @extends {@link BaseError}
+ * @param {Error} message Error message
  */
 exports.InstanceError = function (message) {
 	exports.BaseError.apply(this, arguments);
