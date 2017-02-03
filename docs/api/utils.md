858ffb0c1617002a200921f0bf91d929ec11fc3a
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2017-02-03 08:53:39 -0800

    Utils Documentation

---
+++ docs/api/utils.md
@@ -0,0 +1,81 @@
+# Utils
+
+Interface for some of the Sequelize Utilities exposed in the `Sequelize.Utils` object.
+
+
+
+<a name="uppercaseFirst"></a>
+## uppercaseFirst(str) -> String
+
+Uppercase the first character in a string. Matches Sequelize functionality.
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+str | String | String to uppercase the first character of
+
+
+###  Return
+Returns <String> modified string
+
+
+
+<a name="lowercaseFirst"></a>
+## lowercaseFirst(str) -> String
+
+Lowercase the first character in a string. Matches Sequelize functionality.
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+str | String | String to uppercase the first character of
+
+
+###  Return
+Returns <String> modified string
+
+
+
+<a name="singularize"></a>
+## singularize(str) -> String
+
+Returns the "singular" version of a word. As with Sequelize, this uses the [inflection
+library](https://github.com/dreamerslab/node.inflection) to accomplish this.
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+str | String | Word to convert to its singular form
+
+
+###  Return
+Returns <String> singular version of the given word
+
+
+
+<a name="pluralize"></a>
+## pluralize(str) -> String
+
+Returns the "plural" version of a word. As with Sequelize, this uses the [inflection
+library](https://github.com/dreamerslab/node.inflection) to accomplish this.
+
+###  Parameters
+
+Name | Type | Description
+--- | --- | ---
+str | String | Word to convert to its plural form
+
+
+###  Return
+Returns <String> plural version of the given word
+
+
+
+<a name="lodash"></a>
+## lodash
+
+Exposed version of the lodash library <br>**Alias** _
+
--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,61 @@
 'use strict';
 
+/**
+ * Interface for some of the Sequelize Utilities exposed in the `Sequelize.Utils` object.
+ * 
+ * @name Utils
+ * @fileOverview Utility function interface to match Sequelize
+ */
+
 var inflection = require('inflection');
 
+/**
+ * Uppercase the first character in a string. Matches Sequelize functionality.
+ * 
+ * @param {String} str String to uppercase the first character of
+ * @return {String} modified string
+ **/
 exports.uppercaseFirst = function (str) {
 	return str[0].toUpperCase() + str.slice(1);
 };
+
+/**
+ * Lowercase the first character in a string. Matches Sequelize functionality.
+ * 
+ * @param {String} str String to uppercase the first character of
+ * @return {String} modified string
+ **/
 exports.lowercaseFirst = function (str) {
 	return str[0].toLowerCase() + str.slice(1);
 };
+
+/**
+ * Returns the "singular" version of a word. As with Sequelize, this uses the [inflection
+ * library](https://github.com/dreamerslab/node.inflection) to accomplish this.
+ * 
+ * @param {String} str Word to convert to its singular form
+ * @return {String} singular version of the given word
+ **/
 exports.singularize = function(str) {
 	return inflection.singularize(str);
 };
+
+/**
+ * Returns the "plural" version of a word. As with Sequelize, this uses the [inflection
+ * library](https://github.com/dreamerslab/node.inflection) to accomplish this.
+ * 
+ * @param {String} str Word to convert to its plural form
+ * @return {String} plural version of the given word
+ **/
 exports.pluralize = function(str) {
 	return inflection.pluralize(str);
 };
+
+/**
+ * Exposed version of the lodash library
+ * 
+ * @name lodash
+ * @alias _
+ * @property
+ **/
 exports._ = exports.lodash = require('lodash');
